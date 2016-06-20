66afd9090e369b3f9930bb16b83761229be3b8fd
godoing <20141105063T@mac04.local>
2016-06-20 11:07:26 +0800

    1=100
    通过视频的学习，完成这次变量最简单的应用。结合以前最简单的循环完成1-100的加法。编程过程中因为空格问题出错需要注意。

---
+++ main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  learn02
+//
+//  Created by 20141105063T on 16/6/20.
+//  Copyright © 2016年 20141105063T. All rights reserved.
+//
+
+import Foundation
+var a = 1
+var b = 1
+while b<100{
+    b++
+    a=a+b}
+
+
+print(a)
+
